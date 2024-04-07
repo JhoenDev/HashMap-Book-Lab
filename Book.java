1cdaec6f55130287431fd3339faecfe7f7f62d23
Jhoen <118181843+JhoenDev@users.noreply.github.com>
2024-04-07 14:04:57 +0000

    imported from codespace

--- Book.java
+++ Book.java
@@ -1,12 +1,16 @@
 public class Book {
     private String isbn;
     private String title;
-    
+    private Author author;
+
     Book(String isbn, String title) {
         this.title = title;
         this.isbn = isbn;
     }
     
+    public Author getAuthor() {
+        return author;
+    }
     public String getIsbn() {
         return isbn;
     }
@@ -18,5 +22,8 @@ public class Book {
     }
     public void setTitle(String title) {
         this.title = title;
+    }
+    public void setAuthor(Author author) {
+        this.author = author;
     }
 }
--- BookDA.java
+++ BookDA.java
@@ -10,6 +10,8 @@ public class BookDA {
 
     BookDA() throws IOException{
         bookList = new ArrayList<Book>();
+        ArrayList<Author> authorList;
+
         File file = new File("Book.csv");
         FileReader fileReader = new FileReader(file);
         BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -21,6 +23,13 @@ public class BookDA {
         while ((i = fileReader.read()) != -1) {
             value = bufferedReader.readLine().split(",");
             book = new Book(value[0], value[1]);
+
+            if (value[3].equals()) {
+                authorList = new AuthorDA().getAuthorList();
+                authorList
+            }
+
+
             bookList.add(book);
         }
     }
